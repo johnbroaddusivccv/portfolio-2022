dae737578a46616c20793e2ae7008951bbc74437
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2022-07-22 08:05:25 -0500

    heroku

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
 const resumeLink =
-  'https://raw.githubusercontent.com/johnbroaddusivccv/portfolio-2022/master/src/Assets/john_m_broaddusiv.pdf'
+  'https://raw.githubusercontent.com/johnbroaddusivccv/portfolio-2022/master/src/Assets/john_m_broaddus.pdf'
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200)
