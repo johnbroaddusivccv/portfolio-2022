1c6fde8c110a9901637e8e2eb97a47956f726d53
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2022-11-01 23:31:14 -0500

    picture not up to date

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,7 +29,7 @@ function Home() {
             <Col md={5} style={{ paddingBottom: 20 }}>
               <img
                 // src={homeLogo}
-                src='https://media-exp1.licdn.com/dms/image/C5603AQEitrmFjaUIXA/profile-displayphoto-shrink_800_800/0/1608012996510?e=1663200000&v=beta&t=sY5enRptO76-e16harTeSyVPg3smvGPL4kVdZXaqPjg'
+                src='../../Assets/homepic.jpg'
                 alt='home pic'
                 className='img-fluid'
                 style={{ maxHeight: '450px', borderRadius: '50%' }}
